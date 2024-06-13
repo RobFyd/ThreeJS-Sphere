3025273e5efa934e39dfcdadf708b3105d309172
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-06-14 00:10:47 +0100

    sphere created

--- vite/src/main.ts
+++ vite/src/main.ts
@@ -5,4 +5,11 @@ const scene = new THREE.Scene();
 
 // Create sphere
 const geometry = new THREE.SphereGeometry(3, 64, 64);
+const material = new THREE.MeshBasicMaterial({ 
+    color: 0xff0000,
+});
 
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Create camera
