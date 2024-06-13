0938ce91b307020f8a48647f5b191db457b12427
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-06-14 00:45:30 +0100

    light added

--- vite/src/main.ts
+++ vite/src/main.ts
@@ -5,16 +5,21 @@ const scene = new THREE.Scene();
 
 // Create sphere
 const geometry = new THREE.SphereGeometry(3, 64, 64);
-const material = new THREE.MeshBasicMaterial({ 
-    color: 0xff0000,
+const material = new THREE.MeshStandardMaterial({ 
+    color: "lightgreen",
 });
 
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
+// Create light
+const light = new THREE.PointLight(0xffffff, 80, 100)
+light.position.set(5, 10, 10);
+scene.add(light);
+
 // Create camera
-const camera = new THREE.PerspectiveCamera(45, 800, 600)
-camera.position.z = 20;
+const camera = new THREE.PerspectiveCamera(45, 800 / 600)
+camera.position.z = 15;
 scene.add(camera);
 
 // Create renderer
