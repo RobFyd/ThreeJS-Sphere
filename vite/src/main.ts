5d9bcc98b01fb4e9f667af8f97e87772ca30267e
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-06-14 01:14:16 +0100

    refactor: Improve code structure and formatting in main.ts

--- vite/src/main.ts
+++ vite/src/main.ts
@@ -1,13 +1,13 @@
-import * as THREE from 'three';
-import './style.css';
+import * as THREE from "three";
+import "./style.css";
 
 // Create a scene
 const scene = new THREE.Scene();
 
 // Create sphere
 const geometry = new THREE.SphereGeometry(3, 64, 64);
-const material = new THREE.MeshStandardMaterial({ 
-    color: "lightgreen",
+const material = new THREE.MeshStandardMaterial({
+  color: "lightgreen",
 });
 
 const mesh = new THREE.Mesh(geometry, material);
@@ -15,17 +15,22 @@ scene.add(mesh);
 
 // Sizes
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight,
+  width: window.innerWidth,
+  height: window.innerHeight,
 };
-    
+
 // Create light
-const light = new THREE.PointLight(0xffffff, 80, 100)
+const light = new THREE.PointLight(0xffffff, 80, 100);
 light.position.set(5, 10, 10);
 scene.add(light);
 
 // Create camera
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100);
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  100
+);
 camera.position.z = 15;
 scene.add(camera);
 
@@ -37,11 +42,17 @@ renderer.render(scene, camera);
 
 // Resize
 window.addEventListener("resize", () => {
-    // Update sizes
-    sizes.width = window.innerWidth;
-    sizes.height = window.innerHeight;
-    // Update camera
-    camera.aspect = sizes.width / sizes.height;
-    renderer.setSize(sizes.width, sizes.height);
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
 });
-    
+
+const loop = () => {
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+};
+loop();
