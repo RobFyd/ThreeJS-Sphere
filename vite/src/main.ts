06b67a8a639eda450fae5c2b0568f08e09668b97
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-07-13 21:31:06 +0100

    chore: Update color and autoRotateSpeed in main.ts

--- vite/src/main.ts
+++ vite/src/main.ts
@@ -9,7 +9,7 @@ const scene = new THREE.Scene();
 // Create sphere
 const geometry = new THREE.SphereGeometry(3, 64, 64);
 const material = new THREE.MeshStandardMaterial({
-  color: "#00ff00",
+  color: "#CCFF00",
   // wireframe: true,
   // metalness: -0.2,
   roughness: 0.55,
@@ -37,7 +37,7 @@ function updateClock() {
 }
 
 // Create a texture and apply it to the material
-const clockTexture = new THREE.Texture(clockCanvas);
+const clockTexture = new THREE.CanvasTexture(clockCanvas);
 material.map = clockTexture;
 
 // Sizes
@@ -47,7 +47,10 @@ const sizes = {
 };
 
 // Light
-const light = new THREE.PointLight(0xffffff, 100, 100);
+const ambientLight = new THREE.AmbientLight(0xffffff, 50);
+scene.add(ambientLight);
+
+const light = new THREE.PointLight(0xffffff, 1, 100);
 light.position.set(0, 5, 10);
 light.intensity = 500;
 scene.add(light);
@@ -69,7 +72,7 @@ renderer.render(scene, camera);
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 controls.autoRotate = true;
-controls.autoRotateSpeed = 10;
+controls.autoRotateSpeed = -15;
 
 // Resize
 window.addEventListener("resize", () => {
@@ -95,30 +98,30 @@ const loop = () => {
 loop();
 
 // timeline animation
-// const tl = gsap.timeline({ defaults: { duration: 2 } });
-// tl.fromTo(mesh.scale, { x: 0, y: 0, z: 0 }, { x: 1, y: 1, z: 1 });
-// tl.fromTo("nav", { y: "-100%" }, { y: "0%" }, "-=1.5");
-// tl.fromTo(".title", { opacity: 0 }, { opacity: 1 }, "-=1");
+const tl = gsap.timeline({ defaults: { duration: 2 } });
+tl.fromTo(mesh.scale, { x: 0, y: 0, z: 0 }, { x: 1, y: 1, z: 1 });
+tl.fromTo("nav", { y: "-100%" }, { y: "0%" }, "-=1.5");
+tl.fromTo(".title", { opacity: 0 }, { opacity: 1 }, "-=1");
 
 // mouse animation color
-// let mouseDown = false;
-// let rgb = [];
-// window.addEventListener("mousedown", () => (mouseDown = true));
-// window.addEventListener("mouseup", () => (mouseDown = false));
+let mouseDown = false;
+let rgb = [];
+window.addEventListener("mousedown", () => (mouseDown = true));
+window.addEventListener("mouseup", () => (mouseDown = false));
 
-// window.addEventListener("mousemove", (e) => {
-//   if (mouseDown) {
-//     rgb = [
-//       Math.round((e.pageX / sizes.width) * 255),
-//       Math.round((e.pageY / sizes.height) * 255),
-//       150,
-//     ];
-//     // animate color
-//     const newColor = new THREE.Color(`rgb(${rgb.join(",")})`);
-//     gsap.to(mesh.material.color, {
-//       r: newColor.r,
-//       g: newColor.g,
-//       b: newColor.b,
-//     });
-//   }
-// });
+window.addEventListener("mousemove", (e) => {
+  if (mouseDown) {
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.height) * 255),
+      150,
+    ];
+    // animate color
+    const newColor = new THREE.Color(`rgb(${rgb.join(",")})`);
+    gsap.to(mesh.material.color, {
+      r: newColor.r,
+      g: newColor.g,
+      b: newColor.b,
+    });
+  }
+});
